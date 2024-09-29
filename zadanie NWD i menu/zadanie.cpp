16d502c10772f66e4cee5efb4434695de232732c
MichalKorczynski <michalkorczynski0@gmail.com>
2024-09-29 17:16:09 +0200

    Add files via upload

---
+++ zadanie NWD i menu/zadanie.cpp
@@ -0,0 +1,411 @@
+#include iostream
+#include time.h
+#include string
+
+int iloscOcen = 0;
+float oceny[100];
+float ocenyDop[] = { 0.00,1.00,1.50,1.75,2.00,2.50,2.75,3.00,3.50,3.75,4.00,4.50,4.75,5.00,5.50,5.75,6.00 };
+int iloscOceny[] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+float max = 0;
+float min = 6;
+float sumaOcen = 0;
+float sredniaOcen = 0;
+int counter = 0;
+bool found = false;
+
+int findS(stdstring text, char s) {
+    for (int i = 0; i = text.length(); i++) {
+        if (text[i] == s) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void countGrades() {
+    for (int i = 0; i = iloscOcen - 1; i++) {
+        for (int j = 0; j = 16; j++) {
+            if (oceny[i] == ocenyDop[j]) {
+                iloscOceny[j]++;
+            }
+        }
+    }
+}
+
+void zad1() {
+    int wiekPracownikow[16] = { 18,17,21,33,18,17,21,33,18,17,21,33,18,17,21,33 };
+    int sredniWiek = 21;
+
+    double iloscPieniedzyNaKonciePracownika[16] = { 424.51, 843.24, 160.83, 920.34, 638.12, 160.83, 920.34, 638.12,843.24, 160.83, 920.34, 638.12,843.24, 160.83, 920.34, 638.12 };
+    float sredniaIloscPieniedzyNaKoncie = 521.50;
+
+    stdstring imionaPracownikow[16] = { Franek,Tadeusz,Adam,Kacper,Marcin ,Tadeusz,Adam,Kacper,Marcin ,Tadeusz,Adam,Kacper,Marcin ,Tadeusz,Adam,Kacper };
+
+    bool czyPracownikJestPelnioletni[16] = { false,true,true,true,false,true,true,true,false,true,true,true,false,true,true,true };
+
+    char plec[16] = { 'M', 'M', 'M', 'M','M', 'M', 'M', 'M','M', 'M', 'M', 'M','M', 'M', 'M', 'M' };
+
+    stdstring wykonywanePrace[16] = { Spawacz,Lekarz,Sprzedawca,Nauczyciel,Kierowca,Lekarz,Sprzedawca,Nauczyciel,Kierowca,Lekarz,Sprzedawca,Nauczyciel,Kierowca,Lekarz,Sprzedawca,Nauczyciel };
+
+    stdcout  informacje o pracownikach  stdendl;
+    for (int i = 0; i = 15; i++) {
+
+        stdcout  i + 1;
+        if (i + 1  10) {
+            stdcout  .  ;
+        }
+        else if (i + 1 = 10) {
+            stdcout  . ;
+        }
+        stdcout  Imie   imionaPracownikow[i]  , ;
+
+        for (int g = 0; g = 10 - imionaPracownikow[i].length(); g++) {
+            stdcout   ;
+        }
+        stdcout  wiek   wiekPracownikow[i]   ;
+        if (wiekPracownikow[i]  sredniWiek) {
+            stdcout     sredni, ;
+        }
+        else if (wiekPracownikow[i]  sredniWiek) {
+            stdcout     sredni, ;
+        }
+        else {
+            stdcout   == sredni, ;
+        }
+
+        stdcout  Czy pelnioletni ;
+        if (czyPracownikJestPelnioletni[i] == true) {
+            stdcout  Tak, ;
+        }
+        else {
+            stdcout  Nie, ;
+        }
+
+        stdcout  plec ;
+        if (plec[i] == 'M'  plec[i] == 'm') {
+            stdcout  mezczyzna, ;
+        }
+        else if (plec[i] == 'F'  plec[i] == 'f') {
+            stdcout  kobieta, ;
+        }
+        else {
+            stdcout  nieznanainna, ;
+        }
+
+        stdcout  Wykonywana praca   wykonywanePrace[i]  , ;
+        for (int o = 0; o = 15 - wykonywanePrace[i].length(); o++) {
+            stdcout   ;
+        }
+
+        stdcout  Stan Konta   iloscPieniedzyNaKonciePracownika[i]   ;
+        if (iloscPieniedzyNaKonciePracownika[i]  sredniaIloscPieniedzyNaKoncie) {
+            stdcout    sredni, ;
+        }
+        else if (iloscPieniedzyNaKonciePracownika[i]  sredniaIloscPieniedzyNaKoncie) {
+            stdcout    sredni, ;
+        }
+        else {
+            stdcout   == sredni, ;
+        }
+
+        stdcout  stdendl;
+    }
+
+}
+void zad2() {
+
+    stdstring imiona[16] = { Franek,Tadeusz,Adam,Jakub,Kacper,Marcin,Jakub,Adam,Dorota,Aleksandra,Michał,Stefan,Tadeusz,Rafał,Tymon,Szymon };
+    stdstring nazwiska[16] = { Nowak, Kowalski,Tymianek-Kapusta,Kowalski-Nowak,Grzybowski,Pelc,Grzybowski,Pelc,Samiński,Nowak, Kowalski,Tymianek-Kapusta,Kowalski-Nowak,Grzybowski,Pelc,Samiński };
+
+    stdstring adres[16] = { Sosnowa 17, Kminkowa 12, Krakowska 47,Balonowa 18,Jemiolowa 2,Rozana 7,Polna 6,Powstancow slaskich 19,Kminkowa 7,Sosnowa 1,Jemiolowa 8,Krakowska 30,Balonowa 16,Polna 9,Orla 12,Orla 7 };
+
+    stdstring miejscowosc[16] = { Wroclaw, Warszawa, Gdansk ,Łódź,Wroclaw, Warszawa, Gdansk ,Łódź ,Wroclaw, Warszawa, Gdansk ,Łódź ,Wroclaw, Warszawa, Gdansk ,Łódź };
+
+    stdstring pierwszeTrzy;
+    stdstring drugieTrzy;
+
+    for (int i = 0; i = 15; i++) {
+        int kreska = findS(nazwiska[i], '-');
+        pierwszeTrzy = imiona[i].substr(0, 3);
+        if (kreska == -1) {
+            drugieTrzy = nazwiska[i].substr(0, 3);
+        }
+        else {
+            drugieTrzy = nazwiska[i].substr(0, 2);
+            drugieTrzy += nazwiska[i].substr(kreska + 1, 1);
+        }
+
+
+        stdcout  pierwszeTrzy  .   drugieTrzy;
+        stdcout  . adres   adres[i]   ;
+        for (int b = 0; b = 50 - adres[i].length(); b++) {
+            stdcout   ;
+        }
+        stdcout  Miejscowosc  + miejscowosc[i];
+
+        stdcout  stdendl;
+    }
+}
+void zad3() {
+    while (iloscOcen  4  iloscOcen100) {
+        stdcout  Podaj ilosc ocen z przedmiotow(max 100 min 4);
+        stdcin  iloscOcen;
+    }
+
+    for (int i = 0; i = iloscOcen - 1; i++) {
+
+        stdcout  Podaj ocene;
+        stdcin  oceny[i];
+        stdcout  stdendl;
+
+        for (int j = 0; j = 16; j++) {
+            if (oceny[i] == ocenyDop[j]) {
+                found = true;
+            }
+        }
+
+        if (found != true) {
+            stdcout  Podano niedozwolona wartosc oceny  stdendl;
+            i--;
+            continue;
+        }
+        found = false;
+
+        if (oceny[i]  max) {
+            max = oceny[i];
+        }
+        if (oceny[i]  min) {
+            min = oceny[i];
+        }
+    }
+
+    for (int m = 0; m = iloscOcen - 1; m++) {
+        if (oceny[m] != 0)
+        {
+            sumaOcen += oceny[m];
+            counter++;
+        }
+
+    }
+
+    countGrades();
+    sredniaOcen = sumaOcen  counter;
+
+    stdcout  Twoja srednia ocen wynosi   sredniaOcen  stdendl;
+    stdcout  Twoja najwyzsza ocena   max  stdendl;
+    stdcout  Twoja najnizsza ocena   min  stdendl;
+
+    for (int k = 0; k = 16; k++) {
+        if (iloscOceny[k]  0) {
+            stdcout  ocenyDop[k]   wystapila   iloscOceny[k]   razy  stdendl;;
+        }
+    }
+
+
+}
+void zad4() {
+    stdstring names[10];
+    float points[10];
+    float percents[10];
+    int grade[10];
+    int counter = 0;
+    int percentGrades[] = { 39, 54, 69, 85, 99 };
+
+
+    for (int i = 0; i = 30; i++) {
+
+        stdcout  Podaj imie ucznia;
+        stdcin  names[i];
+        stdcout  stdendl;
+        if (names[i] == 0) {
+            break;
+        }
+        else {
+            counter++;
+        }
+
+        stdcout  Podaj punkty uzyskane przez ucznia (od 0 do 30);
+        stdcin  points[i];
+        stdcout  stdendl;
+    }
+    for (int i = 0; i = counter - 1; i++) {
+
+        if (points[i]  0  points[i]  30) {
+            stdcout  Podano niewlasciwa ocene sprobuj ponownie  stdendl;
+            i--;
+            continue;
+        }
+
+        percents[i] = points[i]  30  100;
+
+        if (percents[i]  percentGrades[0]) {
+            grade[i] = 1;
+        }
+        else {
+            for (int f = 1; f = 5; f++) {
+                if (percents[i] = percentGrades[f - 1]) {
+                    grade[i] = f + 1;
+                }
+            }
+        }
+    }
+    for (int j = 0; j = counter - 1; j++) {
+        stdcout  names[j];
+
+        for (int l = 0; l = 20 - names[j].length(); l++) {
+            stdcout   ;
+        }
+
+        stdcout  grade[j];
+        stdcout            ;
+        stdcout  percents[j];
+        stdcout  stdendl;
+    }
+}
+
+void NWD1() { B1.a
+    kr00
+    long long a, b, c; kr01
+    stdcout  Ile pomiarow chesz wykonac ;
+    stdcin  c;
+    if (c = 0) {
+        c = 1;
+    }
+    stdcout  stdendl;
+    double pomiary[c];
+
+
+    long long iteracje[c];
+    int liczby[c];
+
+    stdcout  Podaj dwie liczby ;
+    stdcin  a  b;
+    long long bT = b;
+
+    for (int j = 0; j = c - 1; j++) {
+        if (j != 0) {
+            bT = 2;
+        }
+        liczby[j] = bT;
+    }
+    long long tempB = b;
+    long long tempB2 = b;
+    stdcout  ==================================  stdendl;
+    for (int i = 0; i = c - 1; i++) {
+        b=tempB;
+        if (i != 0) {
+            b += tempB2;
+            stdcoutb!!!!stdendl;
+        }
+        tempB=b;
+        
+        clock_t start = clock(); B2
+        while (a != b) { kr02
+            if (a  b) { kr03
+                b = b - a;
+            }
+            else {
+                a = a - b;
+            }
+            iteracje[i]++;
+        }
+        clock_t end1 = clock();
+
+        double elapsed = static_castdouble(end1 - start)  CLOCKS_PER_SEC;
+
+        pomiary[i]=elapsed1000;
+
+        stdcout   Pomiar   i + 1   NWD (  a  ,  liczby[i]  ) =   a  stdendl; kr04
+        stdcout   Czas      pomiary[i]  stdendl;
+        stdcout   Iteracje      iteracje[i]  stdendl;
+        stdcout  stdendl;
+    }
+    stdcout  ==================================  stdendl;
+    stdcout  stdendl;
+    int czas[c];
+    for(int l = 0; l=c-1;l++) {
+        czas[l]=int(pomiary[l]);
+    }
+    stdcoutWykres czasu wykonaniastdendlod 0ms do 75msstdendl;
+    for (int k = 0; k = c - 1; k++) {
+        for (int h = 0; h = 150; h++) {
+            if (czas[k] == h) {
+                stdcout  ;
+            }
+
+            else {
+                stdcout  .;
+
+            }
+        }
+        stdcout  stdendl;
+    }
+
+
+} kr05
+void NWD2() { B1.b
+    clock_t start2 = clock(); B2
+    int tmp;
+    int a, b; kr01
+    stdcout  Podaj dwie liczby ;
+    stdcin  a  b;
+    while (b != 0) { kr02
+        tmp = b; kr03
+        b = a % b; kr04
+        a = tmp; kr05
+    }
+    stdcout  NWD =   a  stdendl; kr06
+    clock_t end2 = clock();
+    double elapsed2 = double(end2 - start2)  CLOCKS_PER_SEC;
+    stdcout  Czas wykonania   elapsed2  stdendl;
+}
+
+int main() {
+    bool isTrue = true;
+    while (isTrue) {
+        int userInput;
+        stdcout  ==================================  stdendl;
+        stdcout   Wybierz                           stdendl;
+        stdcout  ==================================  stdendl;
+        stdcout   0. Exit                            stdendl;
+        stdcout   1. NWD sp1 + ilosc pomiarow        stdendl;
+        stdcout   2. NWD sp2                         stdendl;
+        stdcout   3. zad1                            stdendl;
+        stdcout   4. zad2                            stdendl;
+        stdcout   5. zad3                            stdendl;
+        stdcout   6. zad4                            stdendl;
+        stdcout  ==================================  stdendl;
+        stdcout  ;
+        stdcin  userInput;
+        stdcout  stdendl;
+
+        switch (userInput)
+        {
+        case 0
+            isTrue = false;
+            break;
+        case 1
+            NWD1();
+            break;
+        case 2
+            NWD2();
+            break;
+        case 3
+            zad1();
+            break;
+        case 4
+            zad2();
+            break;
+        case 5
+            zad3();
+            break;
+        case 6
+            zad4();
+            break;
+        default
+            isTrue = false;
+            break;
+        }
+    }
+
+}
