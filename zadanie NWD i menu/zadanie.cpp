eb99775bc39df3a24f4e06f5316bc6fdd76e6cd3
MichalKorczynski <michalkorczynski0@gmail.com>
2024-09-29 17:54:00 +0200

    Update zadanie.cpp

--- zadanie NWD i menu/zadanie.cpp
+++ zadanie NWD i menu/zadanie.cpp
@@ -1,6 +1,6 @@
-#include iostream
-#include time.h
-#include string
+#include <iostream>
+#include <time.h>
+#include <string>
 
 int iloscOcen = 0;
 float oceny[100];
@@ -13,8 +13,8 @@ float sredniaOcen = 0;
 int counter = 0;
 bool found = false;
 
-int findS(stdstring text, char s) {
-    for (int i = 0; i = text.length(); i++) {
+int findS(std::string text, char s) {
+    for (int i = 0; i <= text.length(); i++) {
         if (text[i] == s) {
             return i;
         }
@@ -23,8 +23,8 @@ int findS(stdstring text, char s) {
 }
 
 void countGrades() {
-    for (int i = 0; i = iloscOcen - 1; i++) {
-        for (int j = 0; j = 16; j++) {
+    for (int i = 0; i <= iloscOcen - 1; i++) {
+        for (int j = 0; j <= 16; j++) {
             if (oceny[i] == ocenyDop[j]) {
                 iloscOceny[j]++;
             }
@@ -39,92 +39,92 @@ void zad1() {
     double iloscPieniedzyNaKonciePracownika[16] = { 424.51, 843.24, 160.83, 920.34, 638.12, 160.83, 920.34, 638.12,843.24, 160.83, 920.34, 638.12,843.24, 160.83, 920.34, 638.12 };
     float sredniaIloscPieniedzyNaKoncie = 521.50;
 
-    stdstring imionaPracownikow[16] = { Franek,Tadeusz,Adam,Kacper,Marcin ,Tadeusz,Adam,Kacper,Marcin ,Tadeusz,Adam,Kacper,Marcin ,Tadeusz,Adam,Kacper };
+    std::string imionaPracownikow[16] = { "Franek","Tadeusz","Adam","Kacper","Marcin" ,"Tadeusz","Adam","Kacper","Marcin" ,"Tadeusz","Adam","Kacper","Marcin" ,"Tadeusz","Adam","Kacper" };
 
     bool czyPracownikJestPelnioletni[16] = { false,true,true,true,false,true,true,true,false,true,true,true,false,true,true,true };
 
     char plec[16] = { 'M', 'M', 'M', 'M','M', 'M', 'M', 'M','M', 'M', 'M', 'M','M', 'M', 'M', 'M' };
 
-    stdstring wykonywanePrace[16] = { Spawacz,Lekarz,Sprzedawca,Nauczyciel,Kierowca,Lekarz,Sprzedawca,Nauczyciel,Kierowca,Lekarz,Sprzedawca,Nauczyciel,Kierowca,Lekarz,Sprzedawca,Nauczyciel };
+    std::string wykonywanePrace[16] = { "Spawacz","Lekarz","Sprzedawca","Nauczyciel","Kierowca","Lekarz","Sprzedawca","Nauczyciel","Kierowca","Lekarz","Sprzedawca","Nauczyciel","Kierowca","Lekarz","Sprzedawca","Nauczyciel" };
 
-    stdcout  informacje o pracownikach  stdendl;
-    for (int i = 0; i = 15; i++) {
+    std::cout << "informacje o pracownikach:" << std::endl;
+    for (int i = 0; i <= 15; i++) {
 
-        stdcout  i + 1;
-        if (i + 1  10) {
-            stdcout  .  ;
+        std::cout << i + 1;
+        if (i + 1 < 10) {
+            std::cout << ".  ";
         }
-        else if (i + 1 = 10) {
-            stdcout  . ;
+        else if (i + 1 >= 10) {
+            std::cout << ". ";
         }
-        stdcout  Imie   imionaPracownikow[i]  , ;
+        std::cout << "Imie: " << imionaPracownikow[i] << ", ";
 
-        for (int g = 0; g = 10 - imionaPracownikow[i].length(); g++) {
-            stdcout   ;
+        for (int g = 0; g <= 10 - imionaPracownikow[i].length(); g++) {
+            std::cout << " ";
         }
-        stdcout  wiek   wiekPracownikow[i]   ;
-        if (wiekPracownikow[i]  sredniWiek) {
-            stdcout     sredni, ;
+        std::cout << "wiek: " << wiekPracownikow[i] << " ";
+        if (wiekPracownikow[i] > sredniWiek) {
+            std::cout << " >  sredni, ";
         }
-        else if (wiekPracownikow[i]  sredniWiek) {
-            stdcout     sredni, ;
+        else if (wiekPracownikow[i] < sredniWiek) {
+            std::cout << " <  sredni, ";
         }
         else {
-            stdcout   == sredni, ;
+            std::cout << " == sredni, ";
         }
 
-        stdcout  Czy pelnioletni ;
+        std::cout << "Czy pelnioletni: ";
         if (czyPracownikJestPelnioletni[i] == true) {
-            stdcout  Tak, ;
+            std::cout << "Tak, ";
         }
         else {
-            stdcout  Nie, ;
+            std::cout << "Nie, ";
         }
 
-        stdcout  plec ;
-        if (plec[i] == 'M'  plec[i] == 'm') {
-            stdcout  mezczyzna, ;
+        std::cout << "plec: ";
+        if (plec[i] == 'M' || plec[i] == 'm') {
+            std::cout << "mezczyzna, ";
         }
-        else if (plec[i] == 'F'  plec[i] == 'f') {
-            stdcout  kobieta, ;
+        else if (plec[i] == 'F' || plec[i] == 'f') {
+            std::cout << "kobieta, ";
         }
         else {
-            stdcout  nieznanainna, ;
+            std::cout << "nieznana/inna, ";
         }
 
-        stdcout  Wykonywana praca   wykonywanePrace[i]  , ;
-        for (int o = 0; o = 15 - wykonywanePrace[i].length(); o++) {
-            stdcout   ;
+        std::cout << "Wykonywana praca: " << wykonywanePrace[i] << ", ";
+        for (int o = 0; o <= 15 - wykonywanePrace[i].length(); o++) {
+            std::cout << " ";
         }
 
-        stdcout  Stan Konta   iloscPieniedzyNaKonciePracownika[i]   ;
-        if (iloscPieniedzyNaKonciePracownika[i]  sredniaIloscPieniedzyNaKoncie) {
-            stdcout    sredni, ;
+        std::cout << "Stan Konta: " << iloscPieniedzyNaKonciePracownika[i] << " ";
+        if (iloscPieniedzyNaKonciePracownika[i] > sredniaIloscPieniedzyNaKoncie) {
+            std::cout << " > sredni, ";
         }
-        else if (iloscPieniedzyNaKonciePracownika[i]  sredniaIloscPieniedzyNaKoncie) {
-            stdcout    sredni, ;
+        else if (iloscPieniedzyNaKonciePracownika[i] < sredniaIloscPieniedzyNaKoncie) {
+            std::cout << " < sredni, ";
         }
         else {
-            stdcout   == sredni, ;
+            std::cout << " == sredni, ";
         }
 
-        stdcout  stdendl;
+        std::cout << std::endl;
     }
 
 }
 void zad2() {
 
-    stdstring imiona[16] = { Franek,Tadeusz,Adam,Jakub,Kacper,Marcin,Jakub,Adam,Dorota,Aleksandra,Michał,Stefan,Tadeusz,Rafał,Tymon,Szymon };
-    stdstring nazwiska[16] = { Nowak, Kowalski,Tymianek-Kapusta,Kowalski-Nowak,Grzybowski,Pelc,Grzybowski,Pelc,Samiński,Nowak, Kowalski,Tymianek-Kapusta,Kowalski-Nowak,Grzybowski,Pelc,Samiński };
+    std::string imiona[16] = { "Franek","Tadeusz","Adam","Jakub","Kacper","Marcin","Jakub","Adam","Dorota","Aleksandra","Michał","Stefan","Tadeusz","Rafał","Tymon","Szymon" };
+    std::string nazwiska[16] = { "Nowak", "Kowalski","Tymianek-Kapusta","Kowalski-Nowak","Grzybowski","Pelc","Grzybowski","Pelc","Samiński","Nowak", "Kowalski","Tymianek-Kapusta","Kowalski-Nowak","Grzybowski","Pelc","Samiński" };
 
-    stdstring adres[16] = { Sosnowa 17, Kminkowa 12, Krakowska 47,Balonowa 18,Jemiolowa 2,Rozana 7,Polna 6,Powstancow slaskich 19,Kminkowa 7,Sosnowa 1,Jemiolowa 8,Krakowska 30,Balonowa 16,Polna 9,Orla 12,Orla 7 };
+    std::string adres[16] = { "Sosnowa 17", "Kminkowa 12", "Krakowska 47","Balonowa 18","Jemiolowa 2","Rozana 7","Polna 6","Powstancow slaskich 19","Kminkowa 7","Sosnowa 1","Jemiolowa 8","Krakowska 30","Balonowa 16","Polna 9","Orla 12","Orla 7" };
 
-    stdstring miejscowosc[16] = { Wroclaw, Warszawa, Gdansk ,Łódź,Wroclaw, Warszawa, Gdansk ,Łódź ,Wroclaw, Warszawa, Gdansk ,Łódź ,Wroclaw, Warszawa, Gdansk ,Łódź };
+    std::string miejscowosc[16] = { "Wroclaw", "Warszawa", "Gdansk" ,"Łódź","Wroclaw", "Warszawa", "Gdansk" ,"Łódź" ,"Wroclaw", "Warszawa", "Gdansk" ,"Łódź" ,"Wroclaw", "Warszawa", "Gdansk" ,"Łódź" };
 
-    stdstring pierwszeTrzy;
-    stdstring drugieTrzy;
+    std::string pierwszeTrzy;
+    std::string drugieTrzy;
 
-    for (int i = 0; i = 15; i++) {
+    for (int i = 0; i <= 15; i++) {
         int kreska = findS(nazwiska[i], '-');
         pierwszeTrzy = imiona[i].substr(0, 3);
         if (kreska == -1) {
@@ -136,50 +136,50 @@ void zad2() {
         }
 
 
-        stdcout  pierwszeTrzy  .   drugieTrzy;
-        stdcout  . adres   adres[i]   ;
-        for (int b = 0; b = 50 - adres[i].length(); b++) {
-            stdcout   ;
+        std::cout << pierwszeTrzy << ". " << drugieTrzy;
+        std::cout << ". adres: " << adres[i] << " ";
+        for (int b = 0; b <= 50 - adres[i].length(); b++) {
+            std::cout << " ";
         }
-        stdcout  Miejscowosc  + miejscowosc[i];
+        std::cout << "Miejscowosc: " + miejscowosc[i];
 
-        stdcout  stdendl;
+        std::cout << std::endl;
     }
 }
 void zad3() {
-    while (iloscOcen  4  iloscOcen100) {
-        stdcout  Podaj ilosc ocen z przedmiotow(max 100 min 4);
-        stdcin  iloscOcen;
+    while (iloscOcen < 4 || iloscOcen>100) {
+        std::cout << "Podaj ilosc ocen z przedmiotow(max 100 min 4):";
+        std::cin >> iloscOcen;
     }
 
-    for (int i = 0; i = iloscOcen - 1; i++) {
+    for (int i = 0; i <= iloscOcen - 1; i++) {
 
-        stdcout  Podaj ocene;
-        stdcin  oceny[i];
-        stdcout  stdendl;
+        std::cout << "Podaj ocene:";
+        std::cin >> oceny[i];
+        std::cout << std::endl;
 
-        for (int j = 0; j = 16; j++) {
+        for (int j = 0; j <= 16; j++) {
             if (oceny[i] == ocenyDop[j]) {
                 found = true;
             }
         }
 
         if (found != true) {
-            stdcout  Podano niedozwolona wartosc oceny  stdendl;
+            std::cout << "Podano niedozwolona wartosc oceny" << std::endl;
             i--;
             continue;
         }
         found = false;
 
-        if (oceny[i]  max) {
+        if (oceny[i] > max) {
             max = oceny[i];
         }
-        if (oceny[i]  min) {
+        if (oceny[i] < min) {
             min = oceny[i];
         }
     }
 
-    for (int m = 0; m = iloscOcen - 1; m++) {
+    for (int m = 0; m <= iloscOcen - 1; m++) {
         if (oceny[m] != 0)
         {
             sumaOcen += oceny[m];
@@ -189,22 +189,22 @@ void zad3() {
     }
 
     countGrades();
-    sredniaOcen = sumaOcen  counter;
+    sredniaOcen = sumaOcen / counter;
 
-    stdcout  Twoja srednia ocen wynosi   sredniaOcen  stdendl;
-    stdcout  Twoja najwyzsza ocena   max  stdendl;
-    stdcout  Twoja najnizsza ocena   min  stdendl;
+    std::cout << "Twoja srednia ocen wynosi: " << sredniaOcen << std::endl;
+    std::cout << "Twoja najwyzsza ocena: " << max << std::endl;
+    std::cout << "Twoja najnizsza ocena: " << min << std::endl;
 
-    for (int k = 0; k = 16; k++) {
-        if (iloscOceny[k]  0) {
-            stdcout  ocenyDop[k]   wystapila   iloscOceny[k]   razy  stdendl;;
+    for (int k = 0; k <= 16; k++) {
+        if (iloscOceny[k] > 0) {
+            std::cout << ocenyDop[k] << " wystapila: " << iloscOceny[k] << " razy" << std::endl;;
         }
     }
 
 
 }
 void zad4() {
-    stdstring names[10];
+    std::string names[10];
     float points[10];
     float percents[10];
     int grade[10];
@@ -212,96 +212,94 @@ void zad4() {
     int percentGrades[] = { 39, 54, 69, 85, 99 };
 
 
-    for (int i = 0; i = 30; i++) {
+    for (int i = 0; i <= 30; i++) {
 
-        stdcout  Podaj imie ucznia;
-        stdcin  names[i];
-        stdcout  stdendl;
-        if (names[i] == 0) {
+        std::cout << "Podaj imie ucznia:";
+        std::cin >> names[i];
+        std::cout << std::endl;
+        if (names[i] == "0") {
             break;
         }
         else {
             counter++;
         }
 
-        stdcout  Podaj punkty uzyskane przez ucznia (od 0 do 30);
-        stdcin  points[i];
-        stdcout  stdendl;
+        std::cout << "Podaj punkty uzyskane przez ucznia (od 0 do 30):";
+        std::cin >> points[i];
+        std::cout << std::endl;
     }
-    for (int i = 0; i = counter - 1; i++) {
+    for (int i = 0; i <= counter - 1; i++) {
 
-        if (points[i]  0  points[i]  30) {
-            stdcout  Podano niewlasciwa ocene sprobuj ponownie  stdendl;
+        if (points[i] < 0 || points[i] > 30) {
+            std::cout << "Podano niewlasciwa ocene sprobuj ponownie" << std::endl;
             i--;
             continue;
         }
 
-        percents[i] = points[i]  30  100;
+        percents[i] = points[i] / 30 * 100;
 
-        if (percents[i]  percentGrades[0]) {
+        if (percents[i] < percentGrades[0]) {
             grade[i] = 1;
         }
         else {
-            for (int f = 1; f = 5; f++) {
-                if (percents[i] = percentGrades[f - 1]) {
+            for (int f = 1; f <= 5; f++) {
+                if (percents[i] >= percentGrades[f - 1]) {
                     grade[i] = f + 1;
                 }
             }
         }
     }
-    for (int j = 0; j = counter - 1; j++) {
-        stdcout  names[j];
+    for (int j = 0; j <= counter - 1; j++) {
+        std::cout << names[j];
 
-        for (int l = 0; l = 20 - names[j].length(); l++) {
-            stdcout   ;
+        for (int l = 0; l <= 20 - names[j].length(); l++) {
+            std::cout << " ";
         }
 
-        stdcout  grade[j];
-        stdcout            ;
-        stdcout  percents[j];
-        stdcout  stdendl;
+        std::cout << grade[j];
+        std::cout << "          ";
+        std::cout << percents[j];
+        std::cout << std::endl;
     }
 }
 
-void NWD1() { B1.a
-    kr00
-    long long a, b, c; kr01
-    stdcout  Ile pomiarow chesz wykonac ;
-    stdcin  c;
-    if (c = 0) {
+void NWD1() { //B1.a
+    //kr00
+    long long a, b, c; //kr01
+    std::cout << "Ile pomiarow chesz wykonac: ";
+    std::cin >> c;
+    if (c <= 0) {
         c = 1;
     }
-    stdcout  stdendl;
+    std::cout << std::endl;
+
     double pomiary[c];
-
-
     long long iteracje[c];
     int liczby[c];
 
-    stdcout  Podaj dwie liczby ;
-    stdcin  a  b;
+    std::cout << "Podaj dwie liczby: ";
+    std::cin >> a >> b;
     long long bT = b;
-
-    for (int j = 0; j = c - 1; j++) {
+    long long tempB = b;
+    long long tempB2 = b;
+    for (int j = 0; j <= c - 1; j++) {
         if (j != 0) {
-            bT = 2;
+            bT += tempB;
         }
         liczby[j] = bT;
     }
-    long long tempB = b;
-    long long tempB2 = b;
-    stdcout  ==================================  stdendl;
-    for (int i = 0; i = c - 1; i++) {
-        b=tempB;
+
+    std::cout << "|==================================|" << std::endl;
+    for (int i = 0; i <= c - 1; i++) {
+        b = tempB;
         if (i != 0) {
             b += tempB2;
-            stdcoutb!!!!stdendl;
         }
-        tempB=b;
-        
-        clock_t start = clock(); B2
-        while (a != b) { kr02
-            if (a  b) { kr03
+        tempB = b;
+
+        clock_t start = clock(); //B2
+        while (a != b) { //kr02
+            if (a < b) { //kr03
                 b = b - a;
             }
             else {
@@ -311,98 +309,98 @@ void NWD1() { B1.a
         }
         clock_t end1 = clock();
 
-        double elapsed = static_castdouble(end1 - start)  CLOCKS_PER_SEC;
+        double elapsed = static_cast<double>(end1 - start) / CLOCKS_PER_SEC;
 
-        pomiary[i]=elapsed1000;
 
-        stdcout   Pomiar   i + 1   NWD (  a  ,  liczby[i]  ) =   a  stdendl; kr04
-        stdcout   Czas      pomiary[i]  stdendl;
-        stdcout   Iteracje      iteracje[i]  stdendl;
-        stdcout  stdendl;
+
+        pomiary[i] = elapsed * 1000;
+
+        std::cout << "| Pomiar " << i + 1 << " NWD (" << a << "," << liczby[i] << ") = " << a << std::endl; //kr04
+        std::cout << "| Czas: " << " " << pomiary[i] << std::endl;
+        std::cout << "| Iteracje: " << " " << iteracje[i] << std::endl;
+        std::cout << std::endl;
     }
-    stdcout  ==================================  stdendl;
-    stdcout  stdendl;
+    std::cout << "|==================================|" << std::endl;
+    std::cout << std::endl;
     int czas[c];
-    for(int l = 0; l=c-1;l++) {
-        czas[l]=int(pomiary[l]);
-    }
-    stdcoutWykres czasu wykonaniastdendlod 0ms do 75msstdendl;
-    for (int k = 0; k = c - 1; k++) {
-        for (int h = 0; h = 150; h++) {
+
+    for (int k = 0; k <= c - 1; k++) {
+        czas[k] = pomiary[k];
+        for (int h = 0; h <= 100; h++) {
             if (czas[k] == h) {
-                stdcout  ;
+                std::cout << "*";
             }
 
             else {
-                stdcout  .;
+                std::cout << ".";
 
             }
         }
-        stdcout  stdendl;
+        std::cout << std::endl;
     }
 
 
-} kr05
-void NWD2() { B1.b
-    clock_t start2 = clock(); B2
+} //kr05
+void NWD2() { //B1.b
+    clock_t start2 = clock(); //?B2
     int tmp;
-    int a, b; kr01
-    stdcout  Podaj dwie liczby ;
-    stdcin  a  b;
-    while (b != 0) { kr02
-        tmp = b; kr03
-        b = a % b; kr04
-        a = tmp; kr05
+    int a, b; //kr01
+    std::cout << "Podaj dwie liczby: ";
+    std::cin >> a >> b;
+    while (b != 0) { //kr02
+        tmp = b; //kr03
+        b = a % b; //kr04
+        a = tmp; //kr05
     }
-    stdcout  NWD =   a  stdendl; kr06
+    std::cout << "NWD = " << a << std::endl; //kr06
     clock_t end2 = clock();
-    double elapsed2 = double(end2 - start2)  CLOCKS_PER_SEC;
-    stdcout  Czas wykonania   elapsed2  stdendl;
+    double elapsed2 = double(end2 - start2) / CLOCKS_PER_SEC;
+    std::cout << "Czas wykonania: " << elapsed2 << std::endl;
 }
 
 int main() {
     bool isTrue = true;
     while (isTrue) {
         int userInput;
-        stdcout  ==================================  stdendl;
-        stdcout   Wybierz                           stdendl;
-        stdcout  ==================================  stdendl;
-        stdcout   0. Exit                            stdendl;
-        stdcout   1. NWD sp1 + ilosc pomiarow        stdendl;
-        stdcout   2. NWD sp2                         stdendl;
-        stdcout   3. zad1                            stdendl;
-        stdcout   4. zad2                            stdendl;
-        stdcout   5. zad3                            stdendl;
-        stdcout   6. zad4                            stdendl;
-        stdcout  ==================================  stdendl;
-        stdcout  ;
-        stdcin  userInput;
-        stdcout  stdendl;
+        std::cout << "|==================================|" << std::endl;
+        std::cout << "| Wybierz:                         |" << std::endl;
+        std::cout << "|==================================|" << std::endl;
+        std::cout << "| 0. Exit                          |" << std::endl;
+        std::cout << "| 1. NWD sp1 + ilosc pomiarow      |" << std::endl;
+        std::cout << "| 2. NWD sp2                       |" << std::endl;
+        std::cout << "| 3. zad1                          |" << std::endl;
+        std::cout << "| 4. zad2                          |" << std::endl;
+        std::cout << "| 5. zad3                          |" << std::endl;
+        std::cout << "| 6. zad4                          |" << std::endl;
+        std::cout << "|==================================|" << std::endl;
+        std::cout << ":";
+        std::cin >> userInput;
+        std::cout << std::endl;
 
         switch (userInput)
         {
-        case 0
+        case 0:
             isTrue = false;
             break;
-        case 1
+        case 1:
             NWD1();
             break;
-        case 2
+        case 2:
             NWD2();
             break;
-        case 3
+        case 3:
             zad1();
             break;
-        case 4
+        case 4:
             zad2();
             break;
-        case 5
+        case 5:
             zad3();
             break;
-        case 6
+        case 6:
             zad4();
             break;
-        default
+        default:
             isTrue = false;
             break;
         }
